36b77759df255520ae6ceca9ada418a99a5bfa87
AbhishikthWilson <abhishikthwilson@gmail.com>
2024-06-07 23:51:40 +0530

    search complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddAlbum from './components/AddAlbum';
+import SearchAlbum from './components/SearchAlbum';
 
 function App() {
   return (
     <div >
       <AddAlbum/>
+      <SearchAlbum/>
     </div>
   );
 }
